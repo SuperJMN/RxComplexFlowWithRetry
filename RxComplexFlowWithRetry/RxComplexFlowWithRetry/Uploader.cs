09b5f1685bc034b40e694ebb64b94491406a5158
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-03-05 12:38:06 +0100

    Rearrangement

--- RxComplexFlowWithRetry/RxComplexFlowWithRetry/Uploader.cs
+++ RxComplexFlowWithRetry/RxComplexFlowWithRetry/Uploader.cs
@@ -6,8 +6,9 @@
 
     public class Uploader
     {
-        private static Random random = new Random((int)DateTime.Now.Ticks);
         private const int UploadFailureProbability = 20;
+        private static readonly Random random = new Random((int) DateTime.Now.Ticks);
+        private readonly Subject<UICallback<Item>> failer = new Subject<UICallback<Item>>();
 
         public Uploader(IObservable<Item> items)
         {
@@ -17,31 +18,25 @@
 
         private static bool ShouldThrow
         {
-            get
-            {                
-                return random.Next(0, 100) > 100 - UploadFailureProbability;
-            }
+            get { return random.Next(0, 100) > 100 - UploadFailureProbability; }
         }
 
         public IObservable<UploadResults> UploadedItems { get; private set; }
 
-        public IObservable<UICallback<Item>> Failed
-        {
-            get;
-            private set;
-        }
-        private Subject<UICallback<Item>> failer = new Subject<UICallback<Item>>();
+        public IObservable<UICallback<Item>> Failed { get; private set; }
 
         private IObservable<UploadResults> Upload(Item item)
         {
             return Observable.Timer(TimeSpan.FromSeconds(4))
-                .SelectMany(_ =>
-                {
-                    if (ShouldThrow)
-                        return Observable.Throw<UploadResults>(new Exception("Item invalid"));
-                    else
+                .SelectMany(
+                    _ =>
+                    {
+                        if (ShouldThrow)
+                        {
+                            return Observable.Throw<UploadResults>(new Exception("Item invalid"));
+                        }
                         return Observable.Return(new UploadResults(item));
-                }).Catch((Exception ex) => RetryFailedItem(item));
+                    }).Catch((Exception ex) => RetryFailedItem(item));
         }
 
 
